bbaeedf02f8234aa75ea085526b8e98ab6791fd5
CrazyEjek <myejek@gmail.com>
2024-05-26 02:34:13 +0500

    8_4project

--- src/jsx.js
+++ src/jsx.js
@@ -7,7 +7,7 @@ export default function jsx(tag, attributes = {}, ...children) {
     const element = document.createElement(tag);
     Object.entries(attributes).forEach(([key, value]) => {
       if (key === "class") {
-        element.classList.add(...value.split(" "));
+        element.classList.add(...value.trim().split(" "));
       } else if (key.startsWith("on") && key.toLowerCase() in window) {
         element.addEventListener(key.toLowerCase().substring(2), value);
       } else if (key === "style" && typeof value === "object") {
---
+++ src/scripts/OrderSuccess.jsx
@@ -0,0 +1,6 @@
+export const OrederSuccess = (id) => (
+    <div class="order__wrapper">
+    <h2 class="order__title">Спасибо за заказ!</h2>
+    <p class="order__id">Ваш номер заказа: {id}</p>
+    </div>
+);
